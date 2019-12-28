3ef8a0a082afee4920e16e9af1cf9323c24292d3
pnalawade <prashant.nalawade21@gmail.com>
2019-12-28 15:33:38 +0530

    MVC 5 Globalization

---
+++ MVCLocalization/MVCLocalization/Models/EmployeeModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCLocalization.Models
+{
+    public class EmployeeModel
+    {
+        [Required(ErrorMessageResourceType = typeof(ErrorMsg), ErrorMessageResourceName ="EmpName")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$",ErrorMessageResourceType = typeof(ErrorMsg), ErrorMessageResourceName = "RegExEmpName")]
+        public string Name { get; set; }
+        [Required(ErrorMessageResourceType = typeof(ErrorMsg), ErrorMessageResourceName = "EmpEmail")]
+        [RegularExpression(@"[\w-]+@([\w -]+\.)+[\w-]+", ErrorMessageResourceType = typeof(ErrorMsg), ErrorMessageResourceName = "RegExEmpMail")]
+        public string Email { get; set; }
+        [Required(ErrorMessageResourceType = typeof(ErrorMsg), ErrorMessageResourceName = "Address")]
+        public string Address { get; set; }
+        [Required(ErrorMessageResourceType = typeof(ErrorMsg), ErrorMessageResourceName = "Technology")]
+        public string Technology { get; set; }
+        [Required(ErrorMessageResourceType = typeof(ErrorMsg), ErrorMessageResourceName = "Experience")]
+        public decimal Experience { get; set; }
+        [Required(ErrorMessageResourceType = typeof(ErrorMsg), ErrorMessageResourceName = "ContactNo")]
+        public string ContactNo { get; set; }
+    }
+    
+}
