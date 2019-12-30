6a343b1d755b3853729db81097b4ebee5b321742
Prashant <prashant.nalawade21@gmail.com>
2019-12-30 14:39:35 +0530

    In Memory database (EF Core) with Web API

---
+++ WebAPIInMemoryDB/WebAPIInMemoryDB/APIContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIInMemoryDB.Model;
+
+namespace WebAPIInMemoryDB
+{
+    public class APIContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public APIContext(DbContextOptions options) : base(options)
+        {
+            LoadCategories();
+        }
+
+        public void LoadCategories()
+        {
+            Category category = new Category(){CategoryName = "Category1"};
+            Categories.Add(category);
+            Category category1 = new Category() { CategoryName = "Category2" };
+            Categories.Add(category1);
+        }
+
+        public List<Category> GetCategories()
+        {
+            return Categories.Local.ToList<Category>();
+        }
+    }
+}
---
+++ WebAPIInMemoryDB/WebAPIInMemoryDB/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIInMemoryDB.Model;
+
+namespace WebAPIInMemoryDB.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Category")]
+    public class CategoryController : Controller
+    {
+        private readonly APIContext apiContext;
+
+        public CategoryController(APIContext apiContext)
+        {
+            this.apiContext = apiContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<Category> categories = apiContext.GetCategories();
+            return Ok(categories);
+        }
+    }
+}
